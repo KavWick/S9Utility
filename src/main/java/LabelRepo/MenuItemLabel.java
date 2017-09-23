47ca6d7be9a013add2beaee17b4864015e6bcad1
W.p.k <kavishawickramasinghe@gmail.com>
2017-09-23 22:25:17 +0200

    Initial commit

---
+++ src/main/java/LabelRepo/MenuItemLabel.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LabelRepo;
+
+/**
+ *
+ * @author Kav
+ */
+public class MenuItemLabel {
+    public static String File = "File";
+    public static String Esci = "Esci";
+}
